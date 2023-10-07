d8992307c927126f15b8b4bedc113616ebedca70
r-poornimashetty <r.poornimashetty@gmail.com>
2023-10-07 14:44:13 +0530

    commented the code

--- server.js
+++ server.js
@@ -18,14 +18,14 @@ app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/auth', require('./routes/api/auth'));
 
 // server static assets in production
-if(process.env.NODE_ENV === 'production'){
+// if(process.env.NODE_ENV === 'production'){
       // set static folders
       app.use(express.static('client/build'));
 
       app.get('*', (req, res) =>{
             res.sendFile(path.resolve(__dirname, 'client', 'build','index.html'));
       })
-}
+// }
 
 // app.use(express.static('public'));
 
